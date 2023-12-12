0ce484efa496ff09d2094fbf14be620382de6c94
ChefJ <hiecreo@gmail.com>
2023-12-12 20:47:34 +0800

    Seperated gridscene.h to gridscene.cpp and gridscene.h, to remove the compile error the object reader introduces.

--- template/precomp.h
+++ template/precomp.h
@@ -369,10 +369,8 @@ public:
 	void Tick() {}
 };
 
-#define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
 
 //#include "scene.h"
-#include "tiny_obj_loader.h"
 #include "bvhscene.h"
 #include "kdtreescene.h"
 #include "gridscene.h"
